348b900357c9b7fcdfd9c766ae2a0ac8010082c1
zapboy216 <zapboy216@netzero.com>
2023-12-25 19:25:07 -0500

    Add ContestListComponent and fetch JSON data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import React from 'react';
+import ContestListComponent from './ContestListComponent';
 
 function App() {
+  // Here you will fetch and pass the JSON data
+  const contests = {/* JSON data */};
+
   return (
-    <div>
-     
+    <div className="container">
+      <h1>Sports Contests</h1>
+      <ContestListComponent contests={contests} />
     </div>
   );
 }
 
 export default App;
+
