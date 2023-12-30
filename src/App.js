d6918c83adfd4e0f088ec7366528f67edb917870
zapboy216 <zapboy216@netzero.com>
2023-12-30 07:48:34 -0500

    Refactor App.js to fetch and display NFL odds data

--- src/App.js
+++ src/App.js
@@ -1,67 +1,42 @@
-const axios = require('axios');
+import React, { useState, useEffect } from 'react';
+import apiService from './apiService'; // Import the API service
+// Import other components if necessary
 
+function App() {
+ 
+  const [nflOdds, setNflOdds] = useState([]); // State for NFL odds data
 
-// An api key is emailed to you when you sign up to a plan
-// Get a free API key at https://api.the-odds-api.com/
-const apiKey = process.env.api_key
+  
 
-const sportKey = 'upcoming' // use the sport_key from the /sports endpoint below, or use 'upcoming' to see the next 8 games across all sports
+  // Fetch NFL odds data
+  useEffect(() => {
+    const loadNFLOdds = async () => {
+      const data = await apiService.fetchNFLOdds();
+      setNflOdds(data); // Update the NFL odds state with fetched data
+    };
+    loadNFLOdds();
+  }, []);
 
-const regions = 'us' // uk | us | eu | au. Multiple can be specified if comma delimited
+  return (
+    <div className="container">
+     
+      <section>
+        <h2>NFL Odds</h2>
+        {nflOdds.length > 0 ? (
+          nflOdds.map(odd => (
+            // Assuming 'odd' has properties like 'home_team', 'away_team'. 
+            // Adjust based on your actual data structure.
+            <div key={odd.id}>
+              <h3>{odd.home_team} vs. {odd.away_team}</h3>
+              {/* Render other details from the 'odd' object */}
+            </div>
+          ))
+        ) : (
+          <p>Loading NFL odds...</p>
+        )}
+      </section>
+    </div>
+  );
+}
 
-const markets = 'h2h' // h2h | spreads | totals. Multiple can be specified if comma delimited
-
-const oddsFormat = 'decimal' // decimal | american
-
-const dateFormat = 'iso' // iso | unix
-
-/*
-    First get a list of in-season sports
-        the sport 'key' from the response can be used to get odds in the next request
-
-*/
-axios.get('https://api.the-odds-api.com/v4/sports', {
-    params: {
-        apiKey
-    }
-})
-.then(response => {
-    console.log(response.data)
-})
-.catch(error => {
-    console.log('Error status', error.response.status)
-    console.log(error.response.data)
-})
-
-
-/*
-    Now get a list of live & upcoming games for the sport you want, along with odds for different bookmakers
-    This will deduct from the usage quota
-    The usage quota cost = [number of markets specified] x [number of regions specified]
-    For examples of usage quota costs, see https://the-odds-api.com/liveapi/guides/v4/#usage-quota-costs
-
-*/
-axios.get(`https://api.the-odds-api.com/v4/sports/${sportKey}/odds`, {
-    params: {
-        apiKey,
-        regions,
-        markets,
-        oddsFormat,
-        dateFormat,
-    }
-})
-.then(response => {
-    // response.data.data contains a list of live and 
-    //   upcoming events and odds for different bookmakers.
-    // Events are ordered by start time (live events are first)
-    console.log(JSON.stringify(response.data))
-
-    // Check your usage
-    console.log('Remaining requests',response.headers['x-requests-remaining'])
-    console.log('Used requests',response.headers['x-requests-used'])
-
-})
-.catch(error => {
-    console.log('Error status', error.response.status)
-    console.log(error.response.data)
-})
+export default App;
